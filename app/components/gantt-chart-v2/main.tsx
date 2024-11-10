2f6a7a277b300ab5ccfed4c28a0996f2a5f3a6f8
Chamesss <chamsedin.azouz@gmail.com>
2024-11-10 19:19:58 +0100

    document not defined fixed

--- app/components/gantt-chart-v2/main.tsx
+++ app/components/gantt-chart-v2/main.tsx
@@ -1,17 +1,25 @@
 'use client'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { DependencyManager } from './components/dependency-manager'
 import { GanttChart } from './components/gantt-chart'
 import { TaskList } from './components/task-list'
 import { ViewSelector } from './components/view-selector'
 
 export default function Gantt() {
+    const [docEnv, setDocEnv] = React.useState(false)
+
+    useEffect(() => {
+        if (typeof document !== 'undefined') {
+            setDocEnv(true)
+        }
+    }, [])
+
     return (
         <div className="h-fit rounded-xl bg-gray-50">
             <h1 className="mt-8 w-full text-center text-4xl font-semibold">
                 Gantt Chart
             </h1>
-            <div className="mx-auto max-w-[1400px] p-8">
+            <div className="mx-auto max-w-[1400px] overflow-x-auto p-8">
                 <div className="overflow-hidden rounded-lg bg-white shadow-lg">
                     <div className="flex items-center justify-between border-b border-gray-200 px-6 py-8">
                         <h1 className="text-2xl font-bold text-gray-900">
@@ -25,7 +33,7 @@ export default function Gantt() {
                             <DependencyManager />
                         </div>
                         <div className="flex-1 overflow-x-auto border-l border-gray-200">
-                            <GanttChart />
+                            {docEnv && <GanttChart />}
                         </div>
                     </div>
                 </div>
